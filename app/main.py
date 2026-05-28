2ebd17537a303d5096d409ab4f0cfba10d8441aa
unknown <hmy1304@kbu.ac.kr>
2026-05-28 17:51:03 +0900

    update

--- app/main.py
+++ app/main.py
@@ -3,10 +3,12 @@ from fastapi.responses import HTMLResponse
 from fastapi.templating import Jinja2Templates
 from pydantic import BaseModel
 from pathlib import Path
+
 from motor.motor_asyncio import AsyncIOMotorClient
 
 from app.models import mongodb
 from app.models.book import BookModel
+from app.book_scraper import NaverBookScraper
 
 app = FastAPI()
 
@@ -17,15 +19,48 @@ templates = Jinja2Templates(directory=BASE_DIR / "templates")
 
 @app.get("/", response_class=HTMLResponse)
 async def root(request: Request):
-    book = BookModel(keyword="python", publisher="lizb", price=1200, image="me.png")
-    save_book = await mongodb.engine.save(book)
-    print(save_book.model_dump(), flush=True)
+    # book=BookModel(
+    #     keyword="python",
+    #     publisher="lizb",
+    #     price=1200,
+    #     image="me.png"
+    # )
+    # save_book= await mongodb.engine.save(book)
+    # print(save_book.model_dump(), flush=True)
     return templates.TemplateResponse(request, "index.html", {"title": "북북"})
 
 
 @app.get("/search", response_class=HTMLResponse)
 async def read_item(request: Request, q: str):
-    return templates.TemplateResponse(request, "index.html", {"keyword": q})
+    keyword = q
+
+    if not keyword:
+        return templates.TemplateResponse(
+            request=request,
+            name="index.html",
+            context={"message": "검색어를 입력해주세요"},
+        )
+    naver_book_scraper = NaverBookScraper()
+
+    books = await naver_book_scraper.search(keyword, 10)
+
+    book_models = []
+
+    for book in books:
+        print(book)
+        book_model = BookModel(
+            keyword=keyword,
+            publisher=book["publisher"],
+            price=int(book.get("discount") or 0),
+            image=book["image"],
+        )
+        book_models.append(book_model)
+
+    await mongodb.engine.save_all(book_models)
+
+    return templates.TemplateResponse(
+        request=request, name="index.html", context={"keyword": q, "books": book_models}
+    )
 
 
 @app.on_event("startup")
